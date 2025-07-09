f510dad5ee99efc753929a5d166936fc2a6c22f9
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-07-09 20:04:51 +0530

    firebase

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnnkTh0mq2x0HYuQUU6motP_Idk62vhLw",
+  authDomain: "netflix-clone-reactjsredux.firebaseapp.com",
+  projectId: "netflix-clone-reactjsredux",
+  storageBucket: "netflix-clone-reactjsredux.firebasestorage.app",
+  messagingSenderId: "374691683567",
+  appId: "1:374691683567:web:f309b85a8a303d58000f1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
