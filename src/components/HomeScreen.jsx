a89a7a27dc05e8aa53007cbaf1b7f8af11f4a2b6
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-07-10 00:08:03 +0530

    Navbar is ready

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HomeScreen.css";
+import Nav from './Nav';
+function HomeScreen() {
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default HomeScreen
