b72bcb3ccfcb482ea52f43e832002256e3c35198
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-07-14 17:29:56 +0530

    firebase auth done

---
+++ src/components/ProfileScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./ProfileScreen.css"
+function ProfileScreen() {
+  return (
+    <div className="profileScreen">
+        <h1>test</h1>
+    </div>
+  )
+}
+
+export default ProfileScreen
